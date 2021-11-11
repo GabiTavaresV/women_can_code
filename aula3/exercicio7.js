e4adc043ca6e56f3ad196f9b1373f042ea26d1b8
gabriela.ttavares <gabriela.ttavares@gmail.com>
2021-11-11 17:46:29 -0300

    Exercicios parte 2

---
+++ aula3/exercicio7.js
@@ -0,0 +1,7 @@
+var hojeVaiChover = true;
+
+if (hojeVaiChover = true) {
+    console.log("Leve seu guarda-chuva hoje vai chover")
+}else{
+    console.log("Fique tranquilo, hoje será um dia de sol")
+};
---
+++ aula3/exercicio8.js
@@ -0,0 +1,17 @@
+var maiorQue = 5 > 1;
+var menorQue = 2000000 < 3000000000;
+var igualA = "banana" === "banana";
+var Igual = "50" == 50;
+var diferenteDe = "carro" != "bicicleta";
+var maiorQueA = 50 > 27;
+var menorQueA = 975 <= 1200;
+
+
+
+console.log(maiorQue);
+console.log(menorQue);
+console.log(igualA);
+console.log(Igual);
+console.log(diferenteDe);
+console.log(maiorQueA);
+console.log(menorQueA);
